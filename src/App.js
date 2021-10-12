6bc6bf4e8edeb3f821757ac20f2cf1aa8f00e847
miguelgc95 <miguelgc95@gmail.com>
2021-10-12 17:04:23 +0200

    todo list app

--- src/App.js
+++ src/App.js
@@ -1,9 +1,105 @@
-import './App.css';
+import React, { useState, useEffect } from 'react';
+
+import { TaskBanner } from './components/TaskBanner';
+import { TaskRow } from './components/TaskRow';
+import { TaskCreator } from './components/TaskCreator';
+import { VisibilityControl } from './components/VisibilityControl';
+
 const App = () => {
-  return (
-    <div className="App">
-      <h1>App</h1>
-    </div>
-  );
-}
+    const [taskItems, setTaskItems] = useState([
+        { name: 'task 1', done: false },
+    ]);
+
+    const [showCompleted, setshowCompleted] = useState(false);
+
+    useEffect(() => {
+        let data = localStorage.getItem('tasks');
+        if (data != null) {
+            setTaskItems(JSON.parse(data));
+        } else {
+            setTaskItems([
+                { name: 'task 1 sdfgn', done: false },
+                { name: 'task 2', done: false },
+                { name: 'task 3', done: false },
+            ]);
+            setshowCompleted(true);
+        }
+    }, []);
+
+    useEffect(() => {
+        localStorage.setItem('tasks', JSON.stringify(taskItems));
+    }, [taskItems]);
+
+    const renderItems = doneValue => {
+        return taskItems
+            .filter(task => task.done === doneValue)
+            .map(task => (
+                <TaskRow task={task} key={task.name} toggleTask={toggleTask} />
+            ));
+    };
+
+    const createNewTask = taskName => {
+        if (!taskItems.find(t => t.name === taskName)) {
+            setTaskItems([...taskItems, { name: taskName, done: false }]);
+        }
+    };
+
+    const toggleTask = task => {
+        return setTaskItems(
+            taskItems.map(t =>
+                t.name === task.name ? { ...t, done: !t.done } : t
+            )
+        );
+    };
+
+    return (
+        <div className="App">
+            <TaskBanner taskItems={taskItems} />
+
+            <div className="container-fluid">
+                <TaskCreator callback={createNewTask} />
+
+                <table className="table table-striped table-bordered">
+                    <thead>
+                        <tr>
+                            <th>Description</th>
+                            <th>Done</th>
+                        </tr>
+                    </thead>
+                    <tbody>{renderItems(false)}</tbody>
+                </table>
+
+                <div className="p-4">
+                    <VisibilityControl
+                        description="Completed Tasks"
+                        isChecked={showCompleted}
+                        callback={checked => setshowCompleted(checked)}
+                    />
+                </div>
+                {showCompleted && (
+                    <table className="table table-striped table-bordered">
+                        <thead>
+                            <tr>
+                                <th>Description of completed tasks</th>
+                                <th>Done</th>
+                            </tr>
+                        </thead>
+                        <tbody>{renderItems(true)}</tbody>
+                    </table>
+                )}
+            </div>
+        </div>
+    );
+};
 export default App;
+
+// HACER DE ESTA PÁGINA ALGO DINÁMICO FUNCIONAL, ÚTIL E INTERESANTE
+// Añadir readme al repo del scaling
+// justgetflux.com investigar
+// Pedir cita centro de salud
+// renovación DNI
+// Expedición de título US
+// Certificación académica US
+// cambiar el caps mayus por el esc
+// corregir Desktop con desktop
+// corregir el teclado para poner por dedfecto el inglés, pero añadirle al inglés la posibilidad de poner ñs y acentos
---
+++ src/components/TaskBanner.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export const TaskBanner = props => {
+    return (
+        <>
+            <h4 className="text-center p-4">TODO-List</h4>
+            <p className="text-center">
+                {props.taskItems.filter(t => !t.done).length} tasks to do
+            </p>
+        </>
+    );
+};
